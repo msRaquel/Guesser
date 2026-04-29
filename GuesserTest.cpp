58fb518bc8a07c2373619ce0ea6f7a51b9c494b5
Raquel Zamudio <raquelzamudio4@gmail.com>
2026-04-29 11:59:41 -0700

    Quiz 3 Unit Testing

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -14,9 +14,70 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-// Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
+
+
+// distance tester
+TEST(GuesserTest, empty_string)
 {
-  Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+	Guesser my_guess("ABC");
+	int distance = my_guess.distance("");
+	ASSERT_EQ(3, distance);
+
+}
+
+TEST(GuesserTest, empty_space)
+{
+	Guesser my_guess("ABC");
+	int distance = my_guess.distance("AB C");
+	ASSERT_EQ(2, distance);
+}
+
+TEST(GuesserTest, less_than)
+{
+	Guesser my_guess("ABCD");
+	int distance = my_guess.distance("ABC");
+	ASSERT_EQ(1, distance);
+}
+
+TEST(GuesserTest, equal_length_case)
+{
+	Guesser my_guess("ABCDe");
+	int distance = my_guess.distance("ABCeD");
+	ASSERT_EQ(2, distance);
+}
+TEST(GuesserTest, long_guess)
+{
+	Guesser my_guess("ABCDe");
+	int distance = my_guess.distance("ABCDEFGHIJKLMNOPQRXTUVWXYZ");
+	ASSERT_EQ(5, distance);
+}
+
+TEST(GuesserTest, VerifyRemainingAttempts) {
+    Guesser my_guess("secret");
+
+  
+    my_guess.match("wrong1");
+    my_guess.match("wrong2");
+    my_guess.match("wrong3");
+
+    EXPECT_FALSE(my_guess.match("secret")) << "Object should be locked after 3 failed attempts";
+}
+
+TEST(GuesserTest, verify_match) {
+    string input = "1234567890123456789012345678901234567890";
+    string truncated = "12345678901234567890123456789012";
+    Guesser my_guess(input);
+
+    EXPECT_TRUE(my_guess.match(truncated));
+}
+
+
+TEST(GuesserTest, constructor_test) {
+    string longSecret(40, 'A');
+    Guesser my_guess(longSecret);
+
+  
+   
+    EXPECT_EQ(my_guess.m_remaining, 3);
+    EXPECT_FALSE(my_guess.m_locked);
 }
